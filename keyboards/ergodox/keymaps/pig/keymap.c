010d9963cabb1c90ec4e97d29843f4fe3468eb5a
piglovesyou <thepiglovesyou@gmail.com>
2017-01-09 16:35:08 +0900

    leave original and comment out

--- keyboards/ergodox/keymaps/pig/keymap.c
+++ keyboards/ergodox/keymaps/pig/keymap.c
@@ -15,6 +15,7 @@
 #define MOUSE   2  // mouse mode
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 /* Keymap 0: Windows layer (JIS layout)
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
@@ -58,6 +59,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
     ),
+
+// /* Keymap 0: Windows layer (JIS layout)
+//  *
+//  * ,--------------------------------------------------.           ,--------------------------------------------------.
+//  * | ESC    |   1  |   2  |   3  |   4  |   5  | カナ |           | PSCR |   6  |   7  |   8  |   9  |   0  |   -    |
+//  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+//  * | Tab    |   Q  |   W  |   E  |   R  |   T  | LFn  |           | LFn  |   Y  |   U  |   I  |   O  |   P  |   @`   |
+//  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+//  * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;+  |   :*   |
+//  * |--------+------+------+------+------+------|TOGGLE|           |CTLALT|------+------+------+------+------+--------|
+//  * | LShift |   Z  |   X  |   C  |   V  |   B  |MOUSE |           | DEL  |   N  |   M  |  ,<  |  .>  |   /  |   [{   |
+//  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+//  *   |  cut | paste| copy | Left | Right|                                       |  Up  | Down |  [{  |  }]  |   \  |
+//  *   `----------------------------------'                                       `----------------------------------'
+//  *                                        ,-------------.       ,-------------.
+//  *                                        | LGui | LAlt |       | Alt  | LFn  |
+//  *                                 ,------|------|------|       |------+--------+------.
+//  *                                 |Back  |      | Home |       | PgUp |        |      |
+//  *                                 |space |カナ  |------|       |------| Enter  |Space |
+//  *                                 |      |      | End  |       | PgDn |        |      |
+//  *                                 `--------------------'       `----------------------'
+//  */
+// // If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// // Otherwise, it needs KC_*
+// [BASE] = KEYMAP(  // layer 0 : default
+//         // left hand
+//         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
+//         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MO(FN),
+//         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+//         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   TG(MOUSE),
+//         LCTL(KC_X),     LCTL(KC_V),   LCTL(KC_C),   KC_LEFT,  KC_RGHT,
+//                                                 KC_LGUI,        KC_LALT,
+//                                                                 KC_HOME,
+//                                                 KC_BSPC,KC_GRV, KC_END,
+//         // right hand
+//         KC_PSCR,            KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+//         MO(FN),             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
+//                             KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
+//         LALT(LCTL(KC_DEL)) ,KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RBRC,
+//                                    KC_UP,   KC_DOWN,KC_RBRC,KC_BSLS,          KC_RO,
+//              KC_LALT,        MO(FN),
+//              KC_PGUP,
+//              KC_PGDN,KC_ENT, KC_SPC
+//     ),
 
 /* Keymap 1: Function Layer
  *
